c5057d7408a8aca9ff285deeeeb9dec35b049aed
danikaharu11 <clashhaki55@gmail.com>
2020-10-20 10:53:22 +0800

    Add Nav Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Row from './Row';
 import Banner from './Banner';
 import requests from './requests';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
-
+      <Nav />
       <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
